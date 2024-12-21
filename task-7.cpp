2e7a55fe76ab5510b1ba8941c9ba6bfc8922d413
Artem-14 <76739728+Artem-14@users.noreply.github.com>
2024-12-21 17:11:31 +0300

    Create task-7.cpp

---
+++ task-7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    double sphere_r, diag_p, diag_q;
+    std::cout << "Введите радиус шара и диагонали отверстия (p, q): ";
+    std::cin >> sphere_r >> diag_p >> diag_q;
+
+    if (2 * sphere_r <= sqrt(diag_p * diag_p + diag_q * diag_q)) {
+        std::cout << "Шар пройдет" << std::endl;
+    }
+    else {
+        std::cout << "Шар не пройдет" << std::endl;
+    }
+
+    return 0;
+}
