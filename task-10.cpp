889b2fa50be26a3180a2901b81253d00168026dc
Artem-14 <76739728+Artem-14@users.noreply.github.com>
2024-12-21 17:20:29 +0300

    10

---
+++ task-10.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    int number;
+    std::cout << "Введите трехзначное число: ";
+    std::cin >> number;
+
+    int sum_of_digits = (number / 100) + (number / 10 % 10) + (number % 10);
+    if (sum_of_digits % 3 == 0) {
+        std::cout << "Число делится на 3" << std::endl;
+    }
+    else {
+        std::cout << "Число не делится на 3" << std::endl;
+    }
+
+    return 0;
+}
